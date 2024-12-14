3144ec9e78bc6bc6da737f8762e6ac104f4b7f9f
vannhandz <nhan03102003@gmail.com>
2024-12-14 19:30:23 +0700

    ok

--- src/ss1/Next_Day_Month_Year.java
+++ src/ss1/Next_Day_Month_Year.java
@@ -11,19 +11,15 @@ public class Next_Day_Month_Year {
         int month = sc.nextInt();
         System.out.print("Nhap nam: ");
         int year = sc.nextInt();
-
         boolean checkNamNhuan = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-
         if (((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30) ||
                 ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day <= 31) ||
                 (month == 2 && day < 29) ||
                 (month == 2 && checkNamNhuan && day <= 29)) {
-
-            if (checkNamNhuan && month == 2 && (day == 29||day==28)) {
-                if (day==28)
-                {
-                    System.out.println("tiep theo: " + (day+1) + " " + month  + " " + year);
-                }else {
+            if (checkNamNhuan && month == 2 && (day == 29 || day == 28)) {
+                if (day == 28) {
+                    System.out.println("tiep theo: " + (day + 1) + " " + month + " " + year);
+                } else {
                     System.out.println("tiep theo: " + 1 + " " + (month + 1) + " " + year);
                 }
                 System.out.println("truoc do: " + (day - 1) + " " + month + " " + year);
@@ -32,10 +28,8 @@ public class Next_Day_Month_Year {
                 System.out.println("truoc do: " + 29 + " " + (month - 1) + " " + year);
             } else {
                 if (month == 2 && day == 28) {
-
                     System.out.println("tiep theo: " + 1 + " " + (month + 1) + " " + year);
                     System.out.println("truoc do: " + (day - 1) + " " + month + " " + year);
-
                 } else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 1) {
                     System.out.println("tiep theo: " + (day + 1) + " " + month + " " + year);
                     System.out.println("truoc do: " + 31 + " " + (month - 1) + " " + year);
@@ -63,7 +57,5 @@ public class Next_Day_Month_Year {
         } else {
             System.out.println("ngay thang nam khong hop le");
         }
-
-
     }
 }
