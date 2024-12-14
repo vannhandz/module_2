7ff66ebe0398ac4fca5a93a36909744792261488
vannhandz <nhan03102003@gmail.com>
2024-12-14 21:05:01 +0700

    ok

---
+++ src/ss1/Out_Sum.java
@@ -0,0 +1,16 @@
+package ss1;
+
+import java.util.Scanner;
+
+public class Out_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap n: ");
+        int n = sc.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum+= 1.0 /i;
+        }
+        System.out.println((sum) +" ");
+    }
+}
