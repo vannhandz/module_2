d9aaef93211b5eb430ca11f120c067eb2ac565be
vannhandz <nhan03102003@gmail.com>
2024-12-15 13:44:19 +0700

    ok

---
+++ src/ss1/Out_Sum_2n.java
@@ -0,0 +1,21 @@
+package ss1;
+
+import java.util.Scanner;
+
+public class Out_Sum_2n {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap n: ");
+        int n = sc.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            double check=1;
+            for (int j = 1; j <= (2*i-1); j++) {
+                check*=j;
+            }
+            sum=1.0/check;
+        }
+        System.out.println(sum);
+
+    }
+}
