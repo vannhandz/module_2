23ebba897af2de7e22c6f6de68603a3bc968f905
vannhandz <nhan03102003@gmail.com>
2024-12-18 13:17:47 +0700

    ok

--- src/ss1/Sum_3_Giaithua.java
+++ src/ss1/Sum_3_Giaithua.java
@@ -11,17 +11,23 @@ public class Sum_3_Giaithua {
         int b = sc.nextInt();
         System.out.print("Nhap B: ");
         int c = sc.nextInt();
-        int sum=0;
-        int sumA=1, sumB=1,sumC=1;
-        for (int i = 1; i <=a; i++) {
-           sumA*=i;
+        int sum = 0;
+        int max = 0;
+        if (a >= b && a >= c) max = a;
+        else if (b >= a && b >= c) max = b;
+        else if (c >= a && c >= b) max = c;
+        int sumA = 1, sumB = 1, sumC = 1;
+        for (int i = 1; i <= max; i++) {
+            if (i <= a) {
+                sumA *= i;
+            }
+            if (i <= b) {
+                sumB *= i;
+            }
+            if (i <= c) {
+                sumC *= i;
+            }
         }
-        for (int i = 1; i <=b; i++) {
-            sumB*=i;
-        }
-        for (int i = 1; i <=c; i++) {
-            sumC*=i;
-        }
-        System.out.println("Tong: "+ (sum=sumA+sumB+sumC));
+        System.out.println("Tong: " + (sum = sumA + sumB + sumC));
     }
 }
